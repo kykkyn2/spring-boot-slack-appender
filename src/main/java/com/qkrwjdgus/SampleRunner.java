e0bc8df16c64ffe4f5d32725e1a4aadb29d3375e
jhpark <jh.park@albamapp.com>
2018-10-15 17:15:44 +0900

    spring boot logback slack appender

---
+++ src/main/java/com/qkrwjdgus/SampleRunner.java
@@ -0,0 +1,22 @@
+package com.qkrwjdgus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(WisdomApplication.class);
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        logger.debug("===========");
+        logger.debug("Hello kykkyn2");
+        logger.debug("===========");
+
+    }
+}
